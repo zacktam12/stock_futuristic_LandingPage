2cc84ddc4a0d3ce1aa692325db9c6f9ea1224bfd
zacktam12 <zekariastamiru12@gmail.com>
2025-05-24 23:36:11 +0300

    feat: add DemoSection component and update UseCasesSection with a comment

---
+++ components/DemoSection.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface DemoSectionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const DemoSection: React.FC<DemoSectionProps> = ({ title, children }) => {
+  return (
+    <section
+      style={{
+        margin: "2rem 0",
+        padding: "1rem",
+        border: "1px solid #eee",
+        borderRadius: "8px",
+      }}
+    >
+      <h2 style={{ marginBottom: "1rem" }}>{title}</h2>
+      <div>{children}</div>
+    </section>
+  );
+};
+
+export default DemoSection;
--- components/UseCasesSection.tsx
+++ components/UseCasesSection.tsx
@@ -1,3 +1,5 @@
+// not used
+
 import React from "react";
 
 interface UseCase {
